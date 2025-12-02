3eb9b38609036d43a3721f3ec1cb5079ae7bfb2a
Roei Boribor <roeiboribor@gmail.com>
2025-12-02 08:26:05 +0800

    Update waybill actions to include CancelPrintWaybillAction
    - Added `CancelPrintWaybillAction` to the waybill actions module.
    - Updated the `__all__` export list to include the new action, ensuring it is accessible for import.

--- app/services/waybills/actions/__init__.py
+++ app/services/waybills/actions/__init__.py
@@ -2,5 +2,6 @@ from app.services.waybills.actions.DownloadWaybillAction import DownloadWaybillA
 from app.services.waybills.actions.PrintWaybillAction import PrintWaybillAction
 from app.services.waybills.actions.ChangeStatusAction import ChangeStatusAction
 from app.services.waybills.actions.GetStatusAction import GetStatusAction
+from app.services.waybills.actions.CancelPrintWaybillAction import CancelPrintWaybillAction
 
-__all__ = ['DownloadWaybillAction', 'PrintWaybillAction', 'ChangeStatusAction', 'GetStatusAction']
+__all__ = ['DownloadWaybillAction', 'PrintWaybillAction', 'ChangeStatusAction', 'GetStatusAction', 'CancelPrintWaybillAction']
